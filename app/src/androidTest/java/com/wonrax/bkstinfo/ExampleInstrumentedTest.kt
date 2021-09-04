e6306a25fc745331c0077483a606207343c9a478
wonrax <hahuylonghai2012@gmail.com>
2021-09-04 17:20:21 +0700

    change package name cont.

--- app/src/androidTest/java/com/wonrax/bkstinfo/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/wonrax/bkstinfo/ExampleInstrumentedTest.kt
@@ -1,8 +1,8 @@
-package com.example.bkstinfo
+package com.wonrax.bkstinfo
 
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.platform.app.InstrumentationRegistry
-import org.junit.Assert.assertEquals
+import org.junit.Assert
 import org.junit.Test
 import org.junit.runner.RunWith
 
@@ -17,6 +17,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.example.bkstinfo", appContext.packageName)
+        Assert.assertEquals("com.wonrax.bkstinfo", appContext.packageName)
     }
 }
--- app/src/test/java/com/wonrax/bkstinfo/ExampleUnitTest.kt
+++ app/src/test/java/com/wonrax/bkstinfo/ExampleUnitTest.kt
@@ -1,6 +1,6 @@
-package com.example.bkstinfo
+package com.wonrax.bkstinfo
 
-import org.junit.Assert.assertEquals
+import org.junit.Assert
 import org.junit.Test
 
 /**
@@ -11,6 +11,6 @@ import org.junit.Test
 class ExampleUnitTest {
     @Test
     fun addition_isCorrect() {
-        assertEquals(4, 2 + 2)
+        Assert.assertEquals(4L, (2 + 2).toLong())
     }
 }
