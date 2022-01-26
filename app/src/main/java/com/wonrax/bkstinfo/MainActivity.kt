6e6ce0a7997c632c49a230564bd9e6d18be8c3cb
wonrax <hahuylonghai2012@gmail.com>
2022-01-27 00:43:53 +0700

    refactor

--- app/src/main/java/com/wonrax/bkstinfo/MainActivity.kt
+++ app/src/main/java/com/wonrax/bkstinfo/MainActivity.kt
@@ -24,6 +24,7 @@ import com.wonrax.bkstinfo.ui.theme.BKSTINFOTheme
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setTheme(R.style.BKSTINFO_NoActionBar)
         setContent {
             BKSTINFOTheme {
                 // A surface container using the 'background' color from the theme
