32910cda4c46a9d70b82554fdec3768835d13985
wonrax <hahuylonghai2012@gmail.com>
2022-01-30 00:55:48 +0700

    loading screen

--- app/src/main/java/com/wonrax/bkstinfo/MainActivity.kt
+++ app/src/main/java/com/wonrax/bkstinfo/MainActivity.kt
@@ -4,11 +4,17 @@ import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material.CircularProgressIndicator
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.wonrax.bkstinfo.models.DeviceUser
@@ -43,9 +49,15 @@ class MainActivity : ComponentActivity() {
             BKSTINFOTheme(false) {
                 Surface(
                     modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
+                    color = MaterialTheme.colors.background,
                 ) {
-                    Greeting("Hello, please wait")
+                    Column(
+                        modifier = Modifier.fillMaxWidth().fillMaxHeight(),
+                        verticalArrangement = Arrangement.Center,
+                        horizontalAlignment = Alignment.CenterHorizontally
+                    ) {
+                        CircularProgressIndicator()
+                    }
                 }
             }
         }
