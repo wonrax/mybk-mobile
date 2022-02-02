83e353440406591ed255293dfb93cd79d316e68d
wonrax <hahuylonghai2012@gmail.com>
2022-02-02 18:00:34 +0700

    schedule card compose preview

--- app/src/main/java/com/wonrax/mybk/ui/component/ScheduleCard.kt
+++ app/src/main/java/com/wonrax/mybk/ui/component/ScheduleCard.kt
@@ -11,6 +11,7 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.wonrax.mybk.ui.theme.MybkColors
 import com.wonrax.mybk.viewmodels.CourseSchedule
@@ -37,4 +38,30 @@ fun ScheduleCard(schedule: CourseSchedule) {
             schedule.tuan_hoc?.let { Text("Tuần học: $it") }
         }
     }
+}
+
+@Preview
+@Composable
+fun Preview() {
+    val mockCourse = CourseSchedule(
+        giobd = "7:00",
+        giokt = "8:50",
+        hk_nh = "20212",
+        ma_mh = "CO69420",
+        ma_nhom = "L01",
+        macoso = "BK-CS1",
+        mssv = "1812069",
+        ngay_cap_nhat = "2022-01-25T18:56:24.000+0000",
+        nhomto = "L01",
+        phong1 = "HANGOUT_MEET",
+        so_tin_chi = 3,
+        tc_hp = 3f,
+        ten_hocky = "Học kỳ 2 Năm học 2021 - 2022",
+        ten_mh = "DE CUONG LUAN VAN",
+        thu1 = 3,
+        tiet_bd1 = 2,
+        tiet_kt1 = 3,
+        tuan_hoc = "01|02|03|--|--|06|07|08|09|--|--|--|--|14|15|16|17|18|"
+    )
+    ScheduleCard(schedule = mockCourse)
 }
--- app/src/main/java/com/wonrax/mybk/viewmodels/SchedulesViewModel.kt
+++ app/src/main/java/com/wonrax/mybk/viewmodels/SchedulesViewModel.kt
@@ -13,33 +13,33 @@ import kotlinx.coroutines.launch
 import okhttp3.FormBody
 import okhttp3.RequestBody
 
-class CourseSchedule {
-    val giobd: String? = null
-    val giokt: String? = null
-    val hk_nh: String? = null
-    val ma_mh: String? = null
-    val ma_nhom: String? = null
-    val macoso: String? = null
-    val mssv: String? = null
-    val ngay_cap_nhat: String? = null
-    val nhomto: String? = null
-    val phong1: String? = null
-    val so_tin_chi: Int? = null
-    val tc_hp: Float? = null
-    val ten_hocky: String? = null
-    val ten_mh: String? = null
-    val thu1: Int? = null
-    val tiet_bd1: Int? = null
-    val tiet_kt1: Int? = null
+class CourseSchedule(
+    val giobd: String? = null,
+    val giokt: String? = null,
+    val hk_nh: String? = null,
+    val ma_mh: String? = null,
+    val ma_nhom: String? = null,
+    val macoso: String? = null,
+    val mssv: String? = null,
+    val ngay_cap_nhat: String? = null,
+    val nhomto: String? = null,
+    val phong1: String? = null,
+    val so_tin_chi: Int? = null,
+    val tc_hp: Float? = null,
+    val ten_hocky: String? = null,
+    val ten_mh: String? = null,
+    val thu1: Int? = null,
+    val tiet_bd1: Int? = null,
+    val tiet_kt1: Int? = null,
     val tuan_hoc: String? = null
-}
+)
 
-class SemesterSchedule {
-    val hk_nh: String? = null
-    val ngay_cap_nhat: String? = null
-    val ten_hocky: String? = null
+class SemesterSchedule(
+    val hk_nh: String? = null,
+    val ngay_cap_nhat: String? = null,
+    val ten_hocky: String? = null,
     val tkb: Array<CourseSchedule>? = null
-}
+)
 
 class SchedulesViewModel : ViewModel() {
     val response = mutableStateOf<Array<SemesterSchedule>?>(null)
