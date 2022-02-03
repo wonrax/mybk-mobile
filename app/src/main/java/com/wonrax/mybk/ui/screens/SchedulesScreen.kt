5ac1b6495465bddfdd4b705d154020e7b911578b
wonrax <hahuylonghai2012@gmail.com>
2022-02-03 23:57:07 +0700

    week date fix

--- app/src/main/java/com/wonrax/mybk/ui/screens/SchedulesScreen.kt
+++ app/src/main/java/com/wonrax/mybk/ui/screens/SchedulesScreen.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.CircularProgressIndicator
 import androidx.compose.runtime.Composable
@@ -18,9 +19,8 @@ import com.wonrax.mybk.ui.component.ScheduleCard
 import com.wonrax.mybk.ui.component.Text
 import com.wonrax.mybk.ui.theme.Color
 import com.wonrax.mybk.viewmodel.SchedulesViewModel
-import java.text.SimpleDateFormat
+import java.util.Calendar
 import java.util.Date
-import java.util.Locale
 
 @Composable
 fun SchedulesScreen(schedulesViewModel: SchedulesViewModel) {
@@ -43,12 +43,34 @@ fun SchedulesScreen(schedulesViewModel: SchedulesViewModel) {
             } else {
                 LazyColumn(
                     verticalArrangement = Arrangement.spacedBy(8.dp),
-                    contentPadding = PaddingValues(12.dp, 64.dp)
+                    contentPadding = PaddingValues(12.dp, 72.dp)
                 ) {
                     item {
-                        Text("Giờ học", fontSize = FontSize.Heading)
-                        val w = SimpleDateFormat("w", Locale("vn")).format(Date())
-                        Text("Tuần $w", color = Color.Grey50)
+                        Column(
+                            modifier = Modifier.padding(12.dp, 0.dp)
+                        ) {
+                            Text(
+                                "Giờ học",
+                                fontSize = FontSize.Heading,
+                            )
+
+                            // TODO make this a function
+                            /* Build a calendar suitable to extract ISO8601 week numbers
+                             * (see http://en.wikipedia.org/wiki/ISO_8601_week_number) */
+                            /* Build a calendar suitable to extract ISO8601 week numbers
+                             * (see http://en.wikipedia.org/wiki/ISO_8601_week_number) */
+                            val calendar: Calendar = Calendar.getInstance()
+                            calendar.minimalDaysInFirstWeek = 4
+                            calendar.firstDayOfWeek = Calendar.MONDAY
+
+                            /* Set date */
+                            calendar.time = Date()
+
+                            /* Get ISO8601 week number */
+                            val w = calendar.get(Calendar.WEEK_OF_YEAR)
+
+                            Text("Tuần $w", color = Color.Grey50)
+                        }
                         Spacer(modifier = Modifier.height(16.dp))
                     }
                     schedulesViewModel.response.value?.forEach { semester ->
